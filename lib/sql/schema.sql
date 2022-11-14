97be7c86a6b04b4e231bd6b8ce1cba1564606f7a
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-11-14 23:49:20 +0200

    Update schema.sql

--- lib/sql/schema.sql
+++ lib/sql/schema.sql
@@ -187,6 +187,7 @@ DELIMITER $$
  END $$  
  
   /*Parametros einai o arithmos pou dhlwse o paikths sthn arxh toy guroy*/
+  
 DELIMITER $$
   CREATE OR REPLACE PROCEDURE bluffOnCard(DeclaredNumber varchar(1)) 
   BEGIN
@@ -195,6 +196,20 @@ DELIMITER $$
 	
     SELECT EXISTS(    
 	SELECT tr.card_number
+    from tablo t natural join trapoula tr
+    where t.pos='4' and t.card=tr.card_id and tr.card_number=DeclaredNumber
+    ) INTO isExist;
+    select isExist;
+END $$
+
+DELIMITER $$
+  CREATE OR REPLACE PROCEDURE bluffOnCard(DeclaredNumber varchar(1)) 
+  BEGIN
+    DECLARE isExist tinyint;
+    SET isExist = 0;
+
+    SELECT count(distinct
+    SELECT tr.card_number
     from tablo t natural join trapoula tr
     where t.pos='4' and t.card=tr.card_id and tr.card_number=DeclaredNumber
     ) INTO isExist;
