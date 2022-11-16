a28547b0cbc3a178308e7fab9f1a15a6901b0c94
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-17 00:49:48 +0200

    Create board.php

---
+++ lib/connect/board.php
@@ -0,0 +1,14 @@
+<?php
+
+
+function read_board() {
+	global $mysqli;
+	$sql = 'select * from board';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	return($res->fetch_all(MYSQLI_ASSOC));
+}
+
+
+?>
