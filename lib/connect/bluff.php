36812938e65afd3103a2ebe57b1dac5dcee0e349
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-17 00:30:57 +0200

    Update bluff.php

--- lib/connect/bluff.php
+++ lib/connect/bluff.php
@@ -1,9 +1,12 @@
 <?php
 
+require_once "../lib/dbconnect.php"; 
+
+
 $method= $_SERVER[REQUEST_METHOD];
 
 $request= explode('/',trim($_SERVER['SCRIPT_NAME'],'/'));
-
+$input = json_decode(file_get_contents('php://input'),true);
 
 switch($r=array_shift($request))
 {
@@ -12,6 +15,7 @@ switch($r=array_shift($request))
         {
             case '':
             case 'player':
-            
+            case
+            /player1/1/12345
         }
 }
