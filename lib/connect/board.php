7d304a4d84ac080f03c8c08dcb18764890b27776
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-17 01:02:19 +0200

    Update board.php

--- lib/connect/board.php
+++ lib/connect/board.php
@@ -1,21 +1,10 @@
 <?php
 
 
-
-function show_board() {
-	global $mysqli;
-	
-	$sql = 'select * from board ';
-	$st = $mysqli->prepare($sql);
-	$st->execute();
-	$res = $st->get_result();
-	header('Content-type: application/json');
-	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
-}
 function show_board() {
     global $mysqli;
 	
-	$sql = 'select * from board';
+	$sql = 'select * from tablo';
 	$st = $mysqli->prepare($sql);
 
 	$st->execute();
