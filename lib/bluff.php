e593c74f08319d3b681dc466250f72b9bc4e3694
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-11-17 10:03:35 +0200

    Update bluff.php

--- lib/bluff.php
+++ lib/bluff.php
@@ -19,16 +19,19 @@ switch ($r=array_shift($request)) {
     case 'board' : 
 	switch ($b=array_shift($request)) {
 		case '': break;
-		case null: handle_game($method,null);break;
+		case null: handle_main($method,null);break;
 		//http://localhost/Bluff2/lib/bluff.php/board/show/1
-        case 'show' : 
+        case 'show' : //show and start
 			$c=array_shift($request);
-			handle_game($method, $c);break;			
+			handle_main($method, $c);break;			
 		break;
-		case 'start': 
-					handle_game($method);break;	
-		case 'player': 
-					break;
+		case 'throw': 
+			$c=array_shift($request);
+			$d=array_shift($request);
+			$e=array_shift($request);
+			$f=array_shift($request);
+			handle_throw($method, $c, $d,null,null,null);
+		break;	
 		default: header("HTTP/1.1 404 Not Found");
 				break;
 	}
@@ -38,7 +41,7 @@ switch ($r=array_shift($request)) {
 	exit;
 }
 
-function handle_game($method, $properties) {
+function handle_main($method, $properties) {
     if($method=='GET') {
 		if ($properties!=null){
 			show_board($properties);
@@ -50,6 +53,25 @@ function handle_game($method, $properties) {
 	else if ($method=='POST') {
            reset_board();
     } 
+	else {
+        header('HTTP/1.1405 Method Not Allowed');      
+	}
+}
+function handle_throw($method, $properties0, $properties, $properties2, $properties3, $properties4) {
+    if($method=='GET') {
+		if ($properties2==null && $properties3==null && $properties4==null){
+			move($properties0, $properties);
+		}
+		else if ($properties3==null && $properties4==null){
+			//header('HTTP/1.1405 Method Not Allowed - GET needs 2 parameters');   
+		}
+		else if ($properties4==null){
+			//header('HTTP/1.1405 Method Not Allowed - GET needs 2 parameters');   
+		}	
+		else {
+			header('HTTP/1.1405 Method Not Allowed - GET needs 1-5 parameters');      
+		}		
+	}
 	else {
         header('HTTP/1.1405 Method Not Allowed');      
 	}
