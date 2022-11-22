f38067f1b80d2aad1eabb8bddd023a319c394a9d
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-22 22:08:36 +0200

    Update bluff.js

--- bluff.js
+++ bluff.js
@@ -1,5 +1,5 @@
 //var serverName = 'http://localhost/Bluff2/lib/bluff.php/';
-var serverName =  'https://users.iee.ihu.gr/~it174940/ADISE22_Bluff2/lib/bluff.php';
+var serverName =  'https://users.iee.ihu.gr/~it174940/ADISE22_Bluff2/lib/bluff.php/';
 var functionFlag=null;
 var functionFlag2=null;
 var functionFlag3=null;
