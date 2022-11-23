f9b00f00419382f74300a8b8d13c93760ea9e376
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-23 19:35:21 +0200

    Update dbconnect.php

--- lib/connect/dbconnect.php
+++ lib/connect/dbconnect.php
@@ -15,7 +15,9 @@ if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
 		$pass=null;
         $mysqli = new mysqli($host, $user, $pass, $db);
 }
+
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo gethostname();
 }?>
