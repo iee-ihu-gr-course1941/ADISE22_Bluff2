df41221f8268a668c8c67b3f47a00d23ffc8e00e
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-11-23 22:17:45 +0200

    Update dbconnect.php

--- lib/connect/dbconnect.php
+++ lib/connect/dbconnect.php
@@ -3,13 +3,11 @@ $host='localhost';
 
 require_once "db_upass.php";
 
-$db = 'bluff';
+
 
 $user=$DB_USER;
 $pass=$DB_PASS;
 
-$mysqli = new mysqli($host, $user, $pass, $db,null,'~/mysql/run/mysql.sock');
-/*
 if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
 	$mysqli = new mysqli($host, $user, $pass, $db,null,'~/mysql/run/mysql.sock');
     // '/home/student/it/2003/it032378/mysql/run/mysql.sock'
@@ -17,16 +15,8 @@ if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
         //test
 		$pass=null;
         $mysqli = new mysqli($host, $user, $pass, $db);
-}*/
-/*
-if(gethostname()=='users.iee.ihu.gr') {
-	$mysqli = new mysqli($host, $user, $pass, $db,null,'~/mysql/run/mysql.sock');
-    // '/home/student/it/2003/it032378/mysql/run/mysql.sock'
-} else {
-        //test
-		$pass=null;
-        $mysqli = new mysqli($host, $user, $pass, $db);
-}*/
+}
+
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
