b7273245a27d35a518c595f7aa0a7770b0279640
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-23 18:44:40 +0200

    Update dbconnect.php

--- lib/connect/dbconnect.php
+++ lib/connect/dbconnect.php
@@ -6,15 +6,15 @@ require_once "db_upass.php";
 $user=$DB_USER;
 $pass=$DB_PASS;
 
-
-if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
+$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174940/mysql/run/mysql.sock');
+/*if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
 	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174940/mysql/run/mysql.sock');
     // '/home/student/it/2003/it032378/mysql/run/mysql.sock'
 } else {
         //test
 		$pass=null;
         $mysqli = new mysqli($host, $user, $pass, $db);
-}
+}*/
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
