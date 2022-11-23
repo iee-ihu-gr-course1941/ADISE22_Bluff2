f4ca077d18ccd8b4586262886a8fc24fb94f9955
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-23 21:48:04 +0200

    Update dbconnect.php

--- lib/connect/dbconnect.php
+++ lib/connect/dbconnect.php
@@ -31,5 +31,4 @@ if(gethostname()=='users.iee.ihu.gr') {
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    echo gethostname();
 }?>
