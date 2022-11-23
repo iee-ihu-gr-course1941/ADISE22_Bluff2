461e8186eb64678f86bcc68253383bd8c54025bf
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-11-23 22:17:43 +0200

    Update db_upass.php

--- lib/connect/db_upass.php
+++ lib/connect/db_upass.php
@@ -3,4 +3,9 @@
 	$DB_PASS = 's8umaCRu*';
 	$DB_USER = 'root';
 
+	if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
+		$db = 'bluff';
+	} else {
+	    $db = 'test';
+}	
 ?>
