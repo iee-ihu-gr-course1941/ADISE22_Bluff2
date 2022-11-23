f80193436bf1b328644ccd2acf90fd2258be010f
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-23 18:53:04 +0200

    Update dbconnect.php

--- lib/connect/dbconnect.php
+++ lib/connect/dbconnect.php
@@ -6,7 +6,7 @@ require_once "db_upass.php";
 $user=$DB_USER;
 $pass=$DB_PASS;
 
-$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174940/mysql/run/mysql.sock');
+$mysqli = new mysqli($host, $user, $pass, $db);
 /*if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
 	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174940/mysql/run/mysql.sock');
     // '/home/student/it/2003/it032378/mysql/run/mysql.sock'
