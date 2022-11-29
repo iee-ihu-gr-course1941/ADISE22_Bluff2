aa5e78c80139980e85c9256e6a94e9fdb183cb62
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-11-29 13:43:08 +0200

    Update db_upass.php

--- lib/connect/db_upass.php
+++ lib/connect/db_upass.php
@@ -1,12 +1,24 @@
 <?php
-
-	$DB_PASS = 's8umaCRu*';
-	$DB_USER = 'root';
-	//echo gethostname();
-	
-	if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
+	$protocol = ((!emptyempty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";  
+	$CurPageURL = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];  
+	if (str_contains($CurPageURL, 'it174940')) {
+		$DB_PASS = 's8umaCRu*';
+		$DB_USER = 'root';
+		$dbUsers = "/home/student/it/2017/it174940/mysql/run/mysql.sock";
+		$db = 'bluff';
+	}
+	else if (str_contains($CurPageURL, 'it032378')) {
+		$DB_PASS = '';
+		$DB_USER = 'it032378';
+		$dbUsers = "/home/student/it/2003/it032378/mysql/run/mysql.sock";
+		$db = 'test';
+	}	
+	else{
+		$db = 'test';
+	}
+	/*if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
 		$db = 'bluff';
 	} else {
-	    $db = 'test';
+	    $db = 'test';*/
 }	
 ?>
