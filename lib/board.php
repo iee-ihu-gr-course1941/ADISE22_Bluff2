10d85a42e5e2c31efcb2330ba66b7be828f1d0b4
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-11-29 12:57:03 +0200

    Update board.php

--- lib/board.php
+++ lib/board.php
@@ -16,7 +16,16 @@ function sqlreturnoneitem($sql, $commandd){ //mporei na dextei kai panw apo ena 
 	}
 	//return $row[$item];
 }
-function sqlreturner($sql){
+function sqlreturner($sql, $commander){
+	global $mysqli;	
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	if($res = $st->get_result()){
+		$temp = $res->fetch_all(MYSQLI_ASSOC);
+		successMsg(json_encode($temp),$commander);
+	}
+}
+function sqlreturnerOld($sql, $commander){
 	global $mysqli;	
 	$st = $mysqli->prepare($sql);
 	$st->execute();
@@ -30,31 +39,31 @@ function sqlwithoutreturn($sql){
 }
 function show_board($item) {
 	$sql = 'call show_boardForMe(' . $item . ')';
-	sqlreturner($sql);
+	sqlreturner($sql,'show_board');
 	//sqlreturner('CALL show_board_For_Active_Player()');
 }
 
 function reset_board() {
-	sqlwithoutreturn('call shuffleAll()');
+	sqlwithoutreturn('call shuffleAll()','reset_board');
 }
 function start() {	
-	sqlreturner('CALL start()');	
+	sqlreturner('CALL start()','start');	
 }
 function move($properties,$properties2) {
 	$sql = 'call playerMove(1,' . $properties2 . ')';
-	sqlreturner($sql);	
+	sqlreturner($sql,'move');	
 }
 function manyMoves($properties,$properties0,$properties2,$properties3,$properties4) {
 	$sql = 'call manyMoves(' . $properties . ',' . $properties0 . ',' . $properties2 . ',' . $properties3 . ',' . $properties4 . ')';
-	sqlreturner($sql);	
+	sqlreturner($sql,'manyMoves');	
 }
 function pass() {	
 	sqlwithoutreturn('call playerMove(2,null)');
 }
 function bluff() {
-	sqlreturner('call playerMove(3,null)');
+	sqlreturner('call playerMove(3,null)','bluff');
 }
 function findPlayerTurn() {
-	sqlreturner('SELECT p_turn FROM game_status');
+	sqlreturner('SELECT p_turn FROM game_status','findPlayerTurn');
 }
 ?>
