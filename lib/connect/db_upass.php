359ba42790e91e6b0c7f42d994bad9b7bf097e48
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2022-12-14 03:47:33 +0200

    Update db_upass.php

--- lib/connect/db_upass.php
+++ lib/connect/db_upass.php
@@ -1,26 +1,20 @@
 <?php
-	$CurPageURL = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	if (preg_match('/\bit174940\b/', $CurPageURL)) {
-		$DB_PASS = 's8umaCRu*';
-		$DB_USER = 'root';
-		$dbUsers = "/home/student/it/2017/it174940/mysql/run/mysql.sock";
-		$db = 'bluff';
+	$CurPageURL = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; //Έλεγχος πολλαπλών user σε server και χρήση σε τοπικό υπολογιστή
+	if (preg_match('/\bΣτοιχείο που κάνει match στον server\b/', $CurPageURL)) {
+		$DB_PASS = 'κωδικός';
+		$DB_USER = 'όνομα χρήστη';
+		$dbUsers = "διέυθυνση του mysql.sock της Mysql";
+		$db = 'όνομα βάσης δεδομένων στην Mysql';
 	}
-	else if (preg_match('/\bit032378\b/', $CurPageURL)) {
-		$DB_PASS = '';
-		$DB_USER = 'it032378';
-		$dbUsers = "/home/student/it/2003/it032378/mysql/run/mysql.sock";
-		$db = 'test';
+	else if (preg_match('/\bΣτοιχείο που κάνει match στον server\b/', $CurPageURL)) { //π.χ else if (preg_match('/\bit032378\b/', $CurPageURL)) {
+		$DB_PASS = 'κωδικός'; //π.χ $DB_PASS = ''
+		$DB_USER = 'όνομα χρήστη'; //π.χ $DB_PASS = 'it032378' ή $DB_USER = 'root';υυυυ
+		$dbUsers = "διέυθυνση του mysql.sock της Mysql"; //π.χ $dbUsers = "/home/student/it/2003/it032378/mysql/run/mysql.sock"
+		$db = 'όνομα βάσης δεδομένων στην Mysql'; //π.χ $db = "test"
 	}	
 	else if (preg_match('/\blocalhost\b/', $CurPageURL)) {
-		$DB_PASS = '';
-		$DB_USER = 'root';
-		$db = 'test';
+		$DB_PASS = 'κωδικός';
+		$DB_USER = 'όνομα χρήστη';
+		$db = 'όνομα βάσης δεδομένων στην Mysql';
 	}
-	//echo $CurPageURL;
-	//localhost/Bluff2/lib/connect/db_upass.php
-	/*if(gethostname()=='users.iee.ihu.gr' || gethostname()=='legendmod.ml/adise') {
-		$db = 'bluff';
-	} else {
-	    $db = 'test';*/	
 ?>
